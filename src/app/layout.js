ada1dca81a6710416f8b1664c0fe4151cef9d61e
RanggaDputra <ranggaardokit@gmail.com>
2023-10-29 23:53:29 +0700

    fix bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import { Inter } from 'next/font/google'
 import Head from "next/head";
 import 'bootstrap/dist/css/bootstrap.css'
-import Script from 'next/script'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"/>
+     
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
